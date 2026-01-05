b4c0d37c646165fd097f6c6943e2d508a84d72ed
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2026-01-05 11:09:59 +0300

    Setup Tabs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -12,6 +13,12 @@ export default function Index() {
       <Text className="text-red-500">
         Edit app/index.tsx to edit this screen.
       </Text>
+      <Link href="/onboarding">
+        <Text className="text-blue-500 mt-4">Go to Onboarding</Text>
+      </Link>
+      <Link href="/movie/avengers">
+        <Text className="text-blue-500 mt-4">Avengers Movie</Text>
+      </Link>
     </View>
   );
 }
